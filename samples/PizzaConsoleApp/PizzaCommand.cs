4627e85e5fdcb01bb72ec1a28840c20409c72e0d
Laurens-van-den-Berg <laurensvdberg94@gmail.com>
2018-01-26 15:54:53 +0100

    Add sample application

---
+++ samples/PizzaConsoleApp/PizzaCommand.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleAppBase.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaConsoleApp
+{
+    [Command(Name = "pizza", Description = "A command to order pizza's")]
+    class PizzaCommand : MainCommand
+    {
+        [CommandArgument(1, Name = "type", Description = "The type of pizza you want to order", Required = true)]
+        string PizzaType { get; set; }
+
+        [CommandArgument(2, Name = "amount", Description = "the amount of pizza's")]
+        [Range(0, int.MaxValue)]
+        int AmountOfPizzas { get; set; }
+
+        [CommandOption(Template = "-s|--size", Description = "The size of the pizza")]
+        PizzaSize Size { get; set; }
+
+        public override int OnExecute()
+        {
+            Console.WriteLine($"You've ordered {AmountOfPizzas} {Size} {PizzaType} pizzas");
+            return 0;
+        }
+
+        private enum PizzaSize
+        {
+            Small, Medium, Big
+        }
+    }
+}
---
+++ samples/PizzaConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace PizzaConsoleApp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var app = new MainCommand();
+
+            return app.Execute(args);
+        }
+    }
+}
